f45bf78e1c2de91669821a732bf858247bd25ed3
Nispax Official <nispaxofficial@gmail.com>
2026-03-01 21:03:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import Background from "./components/Background";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <Background />
 
